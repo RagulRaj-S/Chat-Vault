c9cdd772bebec5cb5be3c90d466882021492eba7
Sanjay B <sanjayjothi2000@gmail.com>
2020-08-28 13:32:36 +0530

    Adding files.

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
+// server.js
 var express = require('express');
-var history = require('connect-history-api-fallback')
 var path = require('path');
 var serveStatic = require('serve-static');
 app = express();
-app.use(history({verbose: true}))
-app.use(serveStatic(path.join(__dirname, '/dist')))
+app.use(serveStatic(__dirname + "/dist"));
 var port = process.env.PORT || 5000;
 app.listen(port);
 console.log('server started '+ port);
